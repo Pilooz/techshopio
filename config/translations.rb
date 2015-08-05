28e06685feb256d81ac3857ea6aeeeea4b9b1f8a
Pierre-Gilles Levallois <pgl@erasme.org>
2015-08-05 10:33:56 +0200

    add translation.rb

---
+++ config/translations.rb
@@ -0,0 +1,60 @@
+# International translation for all texts of applications
+# rubocop:disable Metrics/LineLength
+TRANSLATE = {
+  'Welcome on TechShopIO !' => { fr: 'Bienvenue sur TechShopIO !'},
+  'In' => { fr: 'Entrées'},
+  'Out' => { fr: 'Sorties'},
+  'Check-in stuff in TechShop' => { fr: 'Ré-intégration de matériel' },
+  'Check-out stuff' => { fr: 'Emprunts de matériel'},
+  'Adding stuff in TechShop' => { fr: 'Ajouts de matériel à la Techshop'},
+  'type or scan reference' => { fr: 'taper ou scanner un code' },
+  'Generate barecodes' => {fr: 'Générer des codes barres'},
+
+  'Barcodes' => {fr: 'Codes barres'},
+  'Type' => {fr: 'Type de code'},
+  'Micellenous' => {fr: 'Paramètres avancés'},
+  'Module size' => {fr: 'Taille de la matrice'},
+  'Numeric' => {fr: 'valeurs numériques'},
+  'Alphanumeric' => {fr: 'valeurs alpha-numériques'},
+  'Data matrix' => {fr: 'Matrice de données (2D)'},
+  'Background' => {fr: 'Fond'},
+  'Bar color' => {fr: 'Couleur des barres'},
+  'Bar width' => {fr: 'Largeur'},
+  'Bar height' => {fr: 'Hauteur'},
+  'From' => {fr: 'De'},
+  'Size' => {fr: 'Taille'},
+  'Quiet zone module' => {fr: 'Zone de silence'},
+  'Rectangular' => {fr: 'Rectangle'},
+  'From' => {fr: 'De'},
+  'To' => {fr: 'A'},
+  'Generate !' => {fr: 'Générer !'},
+  'Radical' => {fr: 'Radical'},
+  'Used on every barcode' => {fr: 'utilisé pour tous les code barre'},
+  'Name' => {fr: 'Libellé'},
+  'Description' => {fr: 'Description'},
+  'Yes' => {fr: 'Oui'},
+  'No' => {fr: 'Non'},
+  'Tags (comma separated)' => {fr: 'Mots clés (séparés par des vigules)'},
+  'Cancel' => {fr: 'Annuler'},
+  'Add' => {fr: 'Ajouter'},
+  'Help' => {fr: 'Aide en ligne'},
+  'New Item' => {fr: 'Nouveau matériel'}
+
+}
+
+# Translation for online Help and messages
+HELP =  {
+  'help-barcode-chkval' => { en: 'Please check values. You should put numeric values in order to generate several barcodes.',
+                             fr: "Vérifiez vos saisies. Entrez les nombres \\ndans l'ordre croissant pour générer une suite de codes barres."},
+  'help-new-barcode' => { en: 'The scanned barcode that you can modify.', fr: 'Contient le code barre scanné que vous pouvez modifier.'},
+  'help-new-name' => {en: 'A name or short description on item', fr: 'Nom ou libellé du nouveau matériel.'},
+  'help-new-desc' => {en: 'Long item description', fr: 'Description longue.'},
+  'help-new-picture' => {en: 'Picture figuring out item', fr: 'Photo représentant le matériel.'},
+  'help-new-camera' => {en: 'Take a picture with insight camera.', fr: "Prendre une photo avec l'appareil photo intégré."},
+  'help-new-checkout' => {en: "Check 'yes' if the item is out of Techshop", fr: "Cochez 'Oui' si le matériel est sorti de la Techshop."},
+  'help-new-tags' => {en: 'Add every tags you need.', fr: 'Ajouter vos tags.'},
+  'help-new-plus' => {en: 'Click this button to create item in database.', fr: "Enregistrez votre saisie en cliquant sur le bouton 'ajouter'."}
+
+}
+
+# rubocop:enable Metrics/LineLength
