9cb8167db699ac9d7dd25cdb89679140973fb1ab
Pierre-Gilles Levallois <pgl@erasme.org>
2015-10-15 14:53:52 +0200

    adds startup scripts

---
+++ scripts/run.bat
@@ -0,0 +1,4 @@
+REM
+REM Starting Script for Linux and MacOS-
+REM
+bundle exec rackup -o 0.0.0.0 -p 9292
---
+++ scripts/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+# Starting Script for Linux and MacOS-
+#
+bundle exec rackup -o 0.0.0.0 -p 9292
